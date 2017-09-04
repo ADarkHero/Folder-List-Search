f416db9dac0bbb38c414b92d086bcd5e974dd579
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2017-09-04 10:36:16 +0200

    Projektdateien hinzufügen.

---
+++ CopyArticleImages/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.logBox = New System.Windows.Forms.RichTextBox()
+        Me.logBoxButton = New System.Windows.Forms.Button()
+        Me.sourceFolder = New System.Windows.Forms.TextBox()
+        Me.destinationFolder = New System.Windows.Forms.TextBox()
+        Me.fileList = New System.Windows.Forms.TextBox()
+        Me.sourceFolderButton = New System.Windows.Forms.Button()
+        Me.destinationFolderButton = New System.Windows.Forms.Button()
+        Me.fileListButton = New System.Windows.Forms.Button()
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.settingsButton = New System.Windows.Forms.Button()
+        Me.clearErrorLog = New System.Windows.Forms.Button()
+        Me.chooseSourceFolder = New System.Windows.Forms.Button()
+        Me.chooseDestinationFolder = New System.Windows.Forms.Button()
+        Me.chooseFileList = New System.Windows.Forms.Button()
+        Me.exportErrorLog = New System.Windows.Forms.Button()
+        Me.reducesLogging = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'logBox
+        '
+        Me.logBox.Location = New System.Drawing.Point(296, 66)
+        Me.logBox.Name = "logBox"
+        Me.logBox.Size = New System.Drawing.Size(400, 333)
+        Me.logBox.TabIndex = 0
+        Me.logBox.Text = ""
+        '
+        'logBoxButton
+        '
+        Me.logBoxButton.Enabled = False
+        Me.logBoxButton.Location = New System.Drawing.Point(296, 12)
+        Me.logBoxButton.Name = "logBoxButton"
+        Me.logBoxButton.Size = New System.Drawing.Size(400, 35)
+        Me.logBoxButton.TabIndex = 1
+        Me.logBoxButton.Text = "Error Log..."
+        Me.logBoxButton.UseVisualStyleBackColor = True
+        '
+        'sourceFolder
+        '
+        Me.sourceFolder.Location = New System.Drawing.Point(12, 95)
+        Me.sourceFolder.Name = "sourceFolder"
+        Me.sourceFolder.Size = New System.Drawing.Size(250, 20)
+        Me.sourceFolder.TabIndex = 2
+        '
+        'destinationFolder
+        '
+        Me.destinationFolder.Location = New System.Drawing.Point(11, 170)
+        Me.destinationFolder.Name = "destinationFolder"
+        Me.destinationFolder.Size = New System.Drawing.Size(250, 20)
+        Me.destinationFolder.TabIndex = 3
+        '
+        'fileList
+        '
+        Me.fileList.Location = New System.Drawing.Point(11, 255)
+        Me.fileList.Name = "fileList"
+        Me.fileList.Size = New System.Drawing.Size(250, 20)
+        Me.fileList.TabIndex = 4
+        '
+        'sourceFolderButton
+        '
+        Me.sourceFolderButton.Enabled = False
+        Me.sourceFolderButton.Location = New System.Drawing.Point(11, 66)
+        Me.sourceFolderButton.Name = "sourceFolderButton"
+        Me.sourceFolderButton.Size = New System.Drawing.Size(278, 23)
+        Me.sourceFolderButton.TabIndex = 5
+        Me.sourceFolderButton.Text = "Source Folder"
+        Me.sourceFolderButton.UseVisualStyleBackColor = True
+        '
+        'destinationFolderButton
+        '
+        Me.destinationFolderButton.Enabled = False
+        Me.destinationFolderButton.Location = New System.Drawing.Point(11, 141)
+        Me.destinationFolderButton.Name = "destinationFolderButton"
+        Me.destinationFolderButton.Size = New System.Drawing.Size(278, 23)
+        Me.destinationFolderButton.TabIndex = 6
+        Me.destinationFolderButton.Text = "Destination Folder"
+        Me.destinationFolderButton.UseVisualStyleBackColor = True
+        '
+        'fileListButton
+        '
+        Me.fileListButton.Enabled = False
+        Me.fileListButton.Location = New System.Drawing.Point(11, 226)
+        Me.fileListButton.Name = "fileListButton"
+        Me.fileListButton.Size = New System.Drawing.Size(278, 23)
+        Me.fileListButton.TabIndex = 7
+        Me.fileListButton.Text = "File List"
+        Me.fileListButton.UseVisualStyleBackColor = True
+        '
+        'startButton
+        '
+        Me.startButton.Location = New System.Drawing.Point(14, 329)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(275, 100)
+        Me.startButton.TabIndex = 8
+        Me.startButton.Text = "START!"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'settingsButton
+        '
+        Me.settingsButton.Enabled = False
+        Me.settingsButton.Location = New System.Drawing.Point(12, 12)
+        Me.settingsButton.Name = "settingsButton"
+        Me.settingsButton.Size = New System.Drawing.Size(279, 35)
+        Me.settingsButton.TabIndex = 9
+        Me.settingsButton.Text = "Settings"
+        Me.settingsButton.UseVisualStyleBackColor = True
+        '
+        'clearErrorLog
+        '
+        Me.clearErrorLog.Location = New System.Drawing.Point(296, 405)
+        Me.clearErrorLog.Name = "clearErrorLog"
+        Me.clearErrorLog.Size = New System.Drawing.Size(130, 23)
+        Me.clearErrorLog.TabIndex = 10
+        Me.clearErrorLog.Text = "Clear Error Log"
+        Me.clearErrorLog.UseVisualStyleBackColor = True
+        '
+        'chooseSourceFolder
+        '
+        Me.chooseSourceFolder.Location = New System.Drawing.Point(268, 95)
+        Me.chooseSourceFolder.Name = "chooseSourceFolder"
+        Me.chooseSourceFolder.Size = New System.Drawing.Size(20, 20)
+        Me.chooseSourceFolder.TabIndex = 11
+        Me.chooseSourceFolder.Text = "X"
+        Me.chooseSourceFolder.UseVisualStyleBackColor = True
+        '
+        'chooseDestinationFolder
+        '
+        Me.chooseDestinationFolder.Location = New System.Drawing.Point(268, 170)
+        Me.chooseDestinationFolder.Name = "chooseDestinationFolder"
+        Me.chooseDestinationFolder.Size = New System.Drawing.Size(20, 20)
+        Me.chooseDestinationFolder.TabIndex = 12
+        Me.chooseDestinationFolder.Text = "X"
+        Me.chooseDestinationFolder.UseVisualStyleBackColor = True
+        '
+        'chooseFileList
+        '
+        Me.chooseFileList.Location = New System.Drawing.Point(268, 255)
+        Me.chooseFileList.Name = "chooseFileList"
+        Me.chooseFileList.Size = New System.Drawing.Size(20, 20)
+        Me.chooseFileList.TabIndex = 13
+        Me.chooseFileList.Text = "X"
+        Me.chooseFileList.UseVisualStyleBackColor = True
+        '
+        'exportErrorLog
+        '
+        Me.exportErrorLog.Location = New System.Drawing.Point(432, 405)
+        Me.exportErrorLog.Name = "exportErrorLog"
+        Me.exportErrorLog.Size = New System.Drawing.Size(130, 23)
+        Me.exportErrorLog.TabIndex = 14
+        Me.exportErrorLog.Text = "Export Error Log"
+        Me.exportErrorLog.UseVisualStyleBackColor = True
+        '
+        'reducesLogging
+        '
+        Me.reducesLogging.AutoSize = True
+        Me.reducesLogging.Checked = True
+        Me.reducesLogging.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.reducesLogging.Location = New System.Drawing.Point(568, 409)
+        Me.reducesLogging.Name = "reducesLogging"
+        Me.reducesLogging.Size = New System.Drawing.Size(111, 17)
+        Me.reducesLogging.TabIndex = 16
+        Me.reducesLogging.Text = "Reduced Logging"
+        Me.reducesLogging.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 441)
+        Me.Controls.Add(Me.reducesLogging)
+        Me.Controls.Add(Me.exportErrorLog)
+        Me.Controls.Add(Me.chooseFileList)
+        Me.Controls.Add(Me.chooseDestinationFolder)
+        Me.Controls.Add(Me.chooseSourceFolder)
+        Me.Controls.Add(Me.clearErrorLog)
+        Me.Controls.Add(Me.settingsButton)
+        Me.Controls.Add(Me.startButton)
+        Me.Controls.Add(Me.fileListButton)
+        Me.Controls.Add(Me.destinationFolderButton)
+        Me.Controls.Add(Me.sourceFolderButton)
+        Me.Controls.Add(Me.fileList)
+        Me.Controls.Add(Me.destinationFolder)
+        Me.Controls.Add(Me.sourceFolder)
+        Me.Controls.Add(Me.logBoxButton)
+        Me.Controls.Add(Me.logBox)
+        Me.Name = "Form1"
+        Me.Text = "Search folder for files"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents logBox As RichTextBox
+    Friend WithEvents logBoxButton As Button
+    Friend WithEvents sourceFolder As TextBox
+    Friend WithEvents destinationFolder As TextBox
+    Friend WithEvents fileList As TextBox
+    Friend WithEvents sourceFolderButton As Button
+    Friend WithEvents destinationFolderButton As Button
+    Friend WithEvents fileListButton As Button
+    Friend WithEvents startButton As Button
+    Friend WithEvents settingsButton As Button
+    Friend WithEvents clearErrorLog As Button
+    Friend WithEvents chooseSourceFolder As Button
+    Friend WithEvents chooseDestinationFolder As Button
+    Friend WithEvents chooseFileList As Button
+    Friend WithEvents exportErrorLog As Button
+    Friend WithEvents reducesLogging As CheckBox
+End Class
---
+++ CopyArticleImages/Form1.vb
@@ -0,0 +1,132 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim list As String
+    Dim srcFolder, destFolder As String
+    Dim srcFile, destFile As String
+    Dim settings As String = "settings.txt"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Load Settings
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(settings)
+
+        srcFolder = reader.ReadLine
+        destFolder = reader.ReadLine
+        list = reader.ReadLine
+
+        reader.Close()
+
+
+        fileList.Text = list
+        sourceFolder.Text = srcFolder
+        destinationFolder.Text = destFolder
+    End Sub
+
+
+    Private Sub startButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
+        ' Read Settings from Textboxes
+        list = fileList.Text
+        srcFolder = sourceFolder.Text
+        destFolder = destinationFolder.Text
+
+        ' Write Settings to file
+        System.IO.File.WriteAllText(settings, "")
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(settings, True)
+        file.WriteLine(srcFolder)
+        file.WriteLine(destFolder)
+        file.WriteLine(list)
+        file.Close()
+
+
+
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(list)
+        Dim a As String
+        Dim i As Integer = 0                    ' Counts copied files
+        Dim f As Integer = 0                    ' Counts all files
+        logBox.Text = ""                        ' Clear error log
+
+        ' Read filenames from txt and copy them from srcFolder to destFolder
+        Do
+            a = reader.ReadLine
+
+
+            srcFile = srcFolder + a                 ' Define source file name.
+            destFile = destFolder + a               ' Define target file name.
+            Try
+                FileCopy(srcFile, destFile)         ' Copy source to target.
+                If (Not reducesLogging.Checked) Then
+                    i += 1
+                    f += 1                          ' Count the copied files
+                End If
+            Catch ex As Exception
+                If (reducesLogging.Checked) Then
+                    logBox.Text += srcFile + Environment.NewLine
+                Else
+                    logBox.Text += "NOT FOUND - " + srcFile + Environment.NewLine
+                End If
+
+                If (Not reducesLogging.Checked) Then
+                    i += 1
+                End If
+            End Try
+
+
+
+        Loop Until a Is Nothing
+
+
+        If (Not reducesLogging.Checked) Then
+            logBox.Text = "Files copied: " + f.ToString + " / " + i.ToString + Environment.NewLine + Environment.NewLine + logBox.Text
+        End If
+
+        reader.Close()
+    End Sub
+
+    Private Sub clearErrorLog_Click(sender As Object, e As EventArgs) Handles clearErrorLog.Click
+        logBox.Text = ""
+    End Sub
+
+    Private Sub chooseSourceFolder_Click(sender As Object, e As EventArgs) Handles chooseSourceFolder.Click
+        Dim FolderBrowserDialog1 As New FolderBrowserDialog
+
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            sourceFolder.Text = FolderBrowserDialog1.SelectedPath + "\"
+        End If
+    End Sub
+
+    Private Sub configErrorLog_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub exportErrorLog_Click(sender As Object, e As EventArgs) Handles exportErrorLog.Click
+        Dim saveFileDialog1 As New SaveFileDialog()
+        saveFileDialog1.Filter = "Error log|*.txt"
+        saveFileDialog1.Title = "Save error log"
+        saveFileDialog1.ShowDialog()
+
+        If saveFileDialog1.FileName <> "" Then
+            My.Computer.FileSystem.WriteAllText(saveFileDialog1.FileName, logBox.Text, False)
+        End If
+    End Sub
+
+    Private Sub reducedLogging(sender As Object, e As EventArgs) Handles reducesLogging.CheckedChanged
+
+    End Sub
+
+    Private Sub chooseDestinationFolder_Click(sender As Object, e As EventArgs) Handles chooseDestinationFolder.Click
+        Dim FolderBrowserDialog1 As New FolderBrowserDialog
+
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            destinationFolder.Text = FolderBrowserDialog1.SelectedPath + "\"
+        End If
+    End Sub
+
+    Private Sub chooseFileList_Click(sender As Object, e As EventArgs) Handles chooseFileList.Click
+        Dim OpenFileDialog1 As New OpenFileDialog
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            fileList.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+End Class
